e9b13b7970771dca3548063039c7ea35be31f9ed
DucLM1809 <minhductxqt@gmail.com>
2026-07-02 13:40:18 +0700

    fix: group import by module

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Delete, Param, UseGuards } from '@nestjs/common';
-import { ClerkGuard } from '../auth';
-import { AdminGuard } from '../auth';
-import { CurrentUser } from '../auth';
+import { ClerkGuard, AdminGuard, CurrentUser } from '../auth';
 import { AccountService } from './account.service';
 
 @Controller('account')
